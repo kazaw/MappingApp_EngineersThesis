ec0e46ea46fd631cfa5fbb92dfe11ad8df5a924b
Kacper <kacperzawadzki@gmail.com>
2019-11-14 22:13:29 +0100

    Work in progress

--- app/src/main/java/kacper/mappingapp_engineersthesis/MainActivity.kt
+++ app/src/main/java/kacper/mappingapp_engineersthesis/MainActivity.kt
@@ -1,23 +1,51 @@
 package kacper.mappingapp_engineersthesis
 
-import android.content.Intent
+import android.content.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import androidx.preference.PreferenceManager
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
-    var time = 0;
+    private val timerUIUpdated = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            findViewById<TextView>(R.id.textView_timer).apply {
+                text = intent.extras!!.getString("time")
+            }
+            //-----------------------------------------------------------
+            findViewById<TextView>(R.id.textViewAccx).apply {
+                text = intent.extras!!.getString("AccX")
+            }
+            findViewById<TextView>(R.id.textViewAccy).apply {
+                text = intent.extras!!.getString("AccY")
+            }
+            findViewById<TextView>(R.id.textViewAccz).apply {
+                text = intent.extras!!.getString("AccZ")
+            }
+            //----
+            findViewById<TextView>(R.id.textViewRotX).apply {
+                text = intent.extras!!.getString("RotX")
+            }
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar_main))
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -42,18 +70,32 @@ class MainActivity : AppCompatActivity() {
     }
     fun startSettings() {
         val intent = Intent(this, SettingsActivity::class.java).apply {}
+        val startingDelay: Long = 5000
+        intent.putExtra("startingDelay", startingDelay)
         startActivity(intent)
     }
 
     fun showHelp() {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        findViewById<TextView>(R.id.textView_timer).apply {
+            text = sharedPreferences.getString("diameter", "")//działa
+        }
 
     }
 
     fun button_start_onClick(view: View){
+        val button = findViewById<Button>(R.id.button_start)
+        intent = Intent(this, MappingService::class.java)
+        if (button.text == "stop") {
+            this.stopService(intent)
+            button.text = "start"
+        } else {
+            button.text = "stop"
+            this.startService(intent)
+            this.registerReceiver(timerUIUpdated, IntentFilter("MAPPER_UPDATED"))
+        }
 
     }
 
-    fun startTimer(){
 
-    }
 }
